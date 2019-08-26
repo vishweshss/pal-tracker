5947ad588162e64420bf750ad1cd94a3da4def8e
vishweshss <vishweshss@gmail.com>
2019-08-26 22:12:06 +0530

    manifest

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,21 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    String message;
+
+    public WelcomeController(@Value("${welcome.message}") String msg) {
+
+        this.message = msg;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return message;
     }
 }
